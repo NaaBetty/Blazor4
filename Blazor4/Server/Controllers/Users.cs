820d4f4f636f7ccd0f7d5c4639a312db01848856
Naa-Adjekai Mansah Dsane <naa-adjekai@Naas-MacBook-Pro.local>
2021-12-08 16:24:05 +0000

    work in progress

--- Blazor4/Server/Controllers/Users.cs
+++ Blazor4/Server/Controllers/Users.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Blazor4.Shared;
+using Blazor4.Server.Services;
 namespace Blazor4.Server.Controllers
 {
-    public class Users
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
     {
-        public Users()
+        private UserService _userService;
+        public Users(UserService userService)
         {
+            _userService = userService;
+        }
+
+        // GET api/user
+        [HttpGet("")]
+        public ActionResult<List<User>> Getstrings()
+        {
+            return _userService.GetUsers();
+        }
+
+        // GET api/user/5
+        [HttpGet("{id:length(24)}")]
+        public ActionResult<User> GetstringById(string id)
+        {
+            return _userService.GetUser(id);
+        }
+
+        // POST api/user
+        [HttpPost("")]
+        public User Poststring(User user)
+        {
+            _userService.PostUser(user);
+            return user;
+        }
+
+        // PUT api/user/5
+        [HttpPut("{id:length(24)}")]
+        public User Putstring(string id, User newUser)
+        {
+            return _userService.PutUser(id, newUser);
+
+        }
+
+        // DELETE api/user/5
+        [HttpDelete("{id:length(24)}")]
+        public User DeletestringById(string id)
+        {
+            return _userService.DeleteUser(id);
         }
     }
 }
+
--- Blazor4/Shared/User.cs
+++ Blazor4/Shared/User.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace Blazor4.Shared
 {
     public class User
     {
-        public User()
-        {
-        }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement]
+        public string Name { get; set; }
+        [BsonElement]
+        public string Email { get; set; }
+        [BsonElement]
+        public string Password { get; set; }
     }
 }
